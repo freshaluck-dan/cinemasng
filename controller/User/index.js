f3fb600bcb899adc9845603a55fffbc3f7f0fae1
chika334 <ikenyedaniel@gmail.com>
2023-09-09 14:01:17 +0100

    trying to push to firebase

--- controller/User/index.js
+++ controller/User/index.js
@@ -5,7 +5,8 @@ require("dotenv").config();
 async function getUser(req, res) {
   const { email } = req.body;
 
-  const secret = process.env.SECRET_KEY;
+  // const secret = process.env.SECRET_KEY;
+  const secret = "23456789IJHBH@#!^&%%%SDLJGSJHKDN=#$%LAKA";
 
   connection.query(
     "SELECT * FROM users WHERE email = ?",
@@ -18,10 +19,18 @@ async function getUser(req, res) {
           .json({ error: "An error occurred while executing the query" });
       } else {
         console.log("Query results:", results[0]);
-        // const token = jwt.sign(payload, secret, { expiresIn: '1h' });
+
         data = {};
         data["data"] = results[0];
-        // data["token"] = 0; // 0 for user, 1 for admins
+        let payload = {
+          priv: data.priv,
+          email: data.email,
+          refCode: data.refCode,
+        };
+        const token = jwt.sign(payload, secret, { expiresIn: "1h" });
+        data["token"] = token; // 0 for user, 1 for admins
+
+        console.log(token);
         res.status(200).json(data);
       }
 
--- server.js
+++ server.js
@@ -1,19 +1,93 @@
+// const express = require("express");
+// const app = express();
+// const readCSVFile = require("./controller/fileReader");
+// const cron = require("node-cron");
+// const { connection } = require("./middleware/Dbconnection");
+// // const { routes } = require("./routes/user");
+// const router = require("./routes/user");
+// const cors = require("cors");
+// const bodyParser = require("body-parser");
+// const routeUpload = require("./routes/fileUpload");
+// const multer = require("multer");
+// const routegetUpload = require("./routes/getUploads");
+// // const { routes } = require("./routes");
+
+// function activateVirtualEnv() {
+//   return new Promise((resolve, reject) => {
+//     // Replace '12' with the appropriate Node.js version installed in your environment
+//     const nodeVersion = "10";
+//     const command = `source /home/cinejqzm/nodevenv/public_html/flyerbd/${nodeVersion}/bin/activate && cd /home/cinejqzm/nodejs/flyerbd`;
+
+//     exec(command, (error, stdout, stderr) => {
+//       if (error) {
+//         reject(error);
+//       } else {
+//         resolve();
+//       }
+//     });
+//   });
+// }
+
+// function startApp() {
+//   cron.schedule("0 0 * * *", () => {
+//     // cron.schedule("*/10 * * * * *", () => {
+//     readCSVFile();
+//   });
+
+//   const storage = multer.diskStorage({
+//     destination: "uploads/",
+//     filename: (req, file, callback) => {
+//       const originalName = file.originalname;
+//       callback(null, originalName);
+//     },
+//   });
+
+//   const upload = multer({ storage: storage });
+
+//   app.use(bodyParser.json()); // Parse JSON bodies
+//   app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
+//   app.use(cors());
+//   app.use("/flyerbd/api/v1/user", router);
+//   app.use("/flyerbd/api/v1/upload", upload.single("image"), routeUpload);
+//   app.use("/flyerbd/api/v1/img", routegetUpload);
+
+//   const port = 3000;
+
+//   // Connect to the MySQL server
+//   // connection.connect((error) => {
+//   //   if (error) {
+//   //     console.error("Error connecting to MySQL:", error);
+//   //   } else {
+//   //     console.log("Connected to MySQL server");
+//   //   }
+//   // });
+
+//   app.get("/", (req, res) => {
+//     res.send("Hello World!");
+//   });
+
+//   app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}!`);
+//   });
+// }
+
+// activateVirtualEnv()
+//   .then(() => startApp())
+//   .catch((err) => console.error("Error activating virtual environment:", err));
+
 const express = require("express");
 const app = express();
 const readCSVFile = require("./controller/fileReader");
 const cron = require("node-cron");
 const { connection } = require("./middleware/Dbconnection");
-// const { routes } = require("./routes/user");
 const router = require("./routes/user");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const routeUpload = require("./routes/fileUpload");
 const multer = require("multer");
 const routegetUpload = require("./routes/getUploads");
-// const { routes } = require("./routes");
 
 cron.schedule("0 0 * * *", () => {
-  // cron.schedule("*/10 * * * * *", () => {
   readCSVFile();
 });
 
@@ -27,23 +101,14 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-app.use(bodyParser.json()); // Parse JSON bodies
-app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use("/api/v1/user", router);
-app.use("/api/v1/upload", upload.single("image"), routeUpload);
-app.use("/api/v1/img", routegetUpload);
+app.use("/flyerbd/api/v1/user", router);
+app.use("/flyerbd/api/v1/upload", upload.single("image"), routeUpload);
+app.use("/flyerbd/api/v1/img", routegetUpload);
 
-const port = 3000;
-
-// Connect to the MySQL server
-// connection.connect((error) => {
-//   if (error) {
-//     console.error("Error connecting to MySQL:", error);
-//   } else {
-//     console.log("Connected to MySQL server");
-//   }
-// });
+const port = 7000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
